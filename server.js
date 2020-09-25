3ca684d6a217b4a127c135c9ce758fe5004279d6
kengzas1253 <kengzas1253@gmail.com>
2020-09-25 17:06:29 +0700

    success

--- server.js
+++ server.js
@@ -42,7 +42,28 @@ app.post('/users', (req, res)=> {
     res.json({message: "New user added successfully"})
   })
 });
-
+app.put('/users/:id',(req,res)=>{
+  let id=req.params.id;
+  let Name=req.body.Name;
+  let Lastname=req.body.Lastname;
+  let Tel=req.body.Tel;
+  let sql = 
+  `UPDATE user SET Name = '${Name}', 
+  Lastname ='${Lastname}', Tel =${Tel} 
+  WHERE ID = ${id}`;
+  con.query(sql, (err, data, fields)=> {
+    if (err) throw err;
+    res.json({message: "Update user successfully"})
+  })
+});
+app.delete('/users/:id',(req,res)=>{
+  let id=req.params.id;
+  let sql =  `DELETE FROM user WHERE id = ${id}`;
+  con.query(sql, (err, data, fields)=> {
+    if (err) throw err;
+    res.json({message: "Delete user successfully"})
+  })
+});
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}.`);
